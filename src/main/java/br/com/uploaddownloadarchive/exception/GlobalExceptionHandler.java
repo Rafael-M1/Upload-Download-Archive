0ff9bbff0bd170dfd325e0e3d5e5dfb4f116f2bd
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2024-04-30 12:05:31 -0400

    Adicionado tratamento de excecao quando o arquivo de upload exceder o tamanho estipulado

--- src/main/java/br/com/uploaddownloadarchive/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/uploaddownloadarchive/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package br.com.uploaddownloadarchive.exception;
 import java.util.Collections;
 import java.util.Map;
 
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +14,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ArchiveException.class)
-    public ResponseEntity<Map<String, String>> handleException(ArchiveException ex) {
+	public ResponseEntity<Map<String, String>> handleException(ArchiveException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("Error", ex.getMessage()));
+	}
+
+	@ExceptionHandler(FileSizeLimitExceededException.class)
+	public ResponseEntity<Map<String, String>> handleException(FileSizeLimitExceededException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("Error", ex.getMessage()));
+	}
+
+	@ExceptionHandler(SizeLimitExceededException.class)
+	public ResponseEntity<Map<String, String>> handleException(SizeLimitExceededException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("Error", ex.getMessage()));
     }
 }
